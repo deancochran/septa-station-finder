ccd37c34e2f34a89a9316ed50a93fa21efef15df
Dean Cochran <charles.dean.cochran@outlook.com>
2025-04-15 09:30:51 -0400

    pyright lint ignore in pydantic settings

--- app/core/settings.py
+++ app/core/settings.py
@@ -2,6 +2,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+	SECRET_KEY:str
+	ALGORITHM:str
+	ACCESS_TOKEN_EXPIRE_MINUTES:int
+	DATABASE_URL:str
+	REDIS_URL:str
 	model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8',extra='ignore')
 
-settings = Settings()
+settings = Settings() # type: ignore
